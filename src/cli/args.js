715e2e8eb297c9d0e03e2238782346b235323a28
Dmitriy Korzun <d.korzun@andersenlab.com>
2024-10-04 17:10:41 +0300

    feat:add-solution-cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argsAll = process.argv;
+  const argsPrint = argsAll
+    .slice(2)
+    .map((value, index) =>
+      !(index % 2 === 0) ? `is ${value},` : value.slice(2)
+    )
+    .join(' ')
+    .slice(0, -1);
+
+  console.log(argsPrint);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const variablesEnv = process.env;
+  const variablesRSS = Object
+    .entries(variablesEnv)
+    .filter((key) => key[0].startsWith('RSS_'))
+    .map(([key, value]) => `${key}=${value}`)
+    .join('; ')
+
+  console.log(variablesRSS);
 };
 
 parseEnv();
