259a2440f36d4509392e401b5b025b653dfc039e
Dmitriy Korzun <d.korzun@andersenlab.com>
2024-10-06 15:28:04 +0300

    feat:add-solution-streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import fs from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.resolve(__dirname, 'files', 'fileToRead.txt');
+
+  const fileReadStream = fs.createReadStream(filePath, 'utf8');
+
+  fileReadStream.on('data', (data) => process.stdout.write(data));
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import stream from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  const reverseText = new stream.Transform({
+    transform(chunk, encoding, callback) {
+        this.push(chunk.toString().split('').reverse().join('') + '\n');
+        callback();
+    },
+  });
+  process.stdin.pipe(reverseText).pipe(process.stdout);
 };
 
 await transform();
